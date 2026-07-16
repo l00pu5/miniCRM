9c78cab56eea25ce64ee460606fd2645ace3ae9a
l00pu5 <habbig.sebastian@googlemail.com>
2026-07-16 20:37:04 +0200

    Dummy auth middleware implementation

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,1 +1,12 @@
-// TODO: implement
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  // TODO: implement
+  console.log("Auth check");
+  const authHeader = req.headers["authorization"] || req.headers["Authorization"];
+  console.log("Auth header:", authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("Token:", token);
+  // TODO verify token
+  next();
+};
