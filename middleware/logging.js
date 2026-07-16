c1e4a16e499126a520d439c4f5df1187330fd953
l00pu5 <habbig.sebastian@googlemail.com>
2026-07-16 21:58:58 +0200

    Added logging functionality to logging middleware

--- middleware/logging.js
+++ middleware/logging.js
@@ -2,7 +2,7 @@ const fsPromises = require("fs").promises;
 const path = require("path");
 
 const getTimestamp = (date) => {
-  const d_year = date.getYear();
+  const d_year = 1900 + parseInt(date.getYear());
   const d_month = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
   const d_day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
   const d_time_h = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
@@ -11,16 +11,20 @@ const getTimestamp = (date) => {
   return `${d_day}.${d_month}.${d_year} ${d_time_h}:${d_time_h}:${d_time_s}`;
 };
 
-const logger = (req, res, next) => {
-  const log_date = getTimestamp(Date.now());
+const logger = async (req, res, next) => {
+  const log_date = getTimestamp(new Date(Date.now()));
   const log_method = req.method;
   const log_url = req.url;
   const log_url_orig = req.originalUrl;
   const log_body = req.body;
   const log_params = req.params;
-  const logString = `${log_date}: ${log_method} ${log_url} ${log_url_orig} ${log_params} ${log_body}`;
+  const logString = `${log_date} : ${log_method} ${log_url} ${log_url_orig}`;
   console.log(logString);
-  // TODO: write/append log to log file
+  try {
+    await fsPromises.appendFile(path.join(__dirname, "..", "log", "log.txt"), `${logString}\n`);
+  } catch (error) {
+    console.log(error);
+  }
   next();
 };
 
