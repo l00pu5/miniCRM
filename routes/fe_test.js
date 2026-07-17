0663fb000510b5c4840bfff298c1d067cab8ccaa
l00pu5 <habbig.sebastian@googlemai.com>
2026-07-17 23:49:06 +0200

    Added comments / TODOs

--- routes/fe_test.js
+++ routes/fe_test.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const auth_middleware = require("../middleware/authentication.js");
 
 router.get("/login-form", (req, res) => {
+  // render login form
   console.log("-> /login-form (test)");
   res.render("test/login_form");
 });
 
 router.get("/auth", auth_middleware.authenticate, (req, res) => {
+  // test endpoint for authentication middleware
+  // renders a test page is auth is successful
   console.log("-> /auth (test)");
   res.render("test/authSuccess");
 });
