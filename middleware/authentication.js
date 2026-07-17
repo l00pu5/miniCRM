b4cba228ee5715e2a7ffdc325580312c7e8099e7
l00pu5 <habbig.sebastian@googlemail.com>
2026-07-17 06:30:14 +0200

    Refined auth middleware implementation

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,12 +1,36 @@
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  // TODO: implement
   console.log("Auth check");
   const authHeader = req.headers["authorization"] || req.headers["Authorization"];
   console.log("Auth header:", authHeader);
-  const token = authHeader && authHeader.split(" ")[1];
+  if (!authHeader) {
+    console.error("No auth header is present!");
+    res.status(401).json({ message: "No token has been provided!" });
+    return;
+  }
+  const headerComponents = authHeader.split(" ");
+  if (authHeader.length !== 2) {
+    // auth header is formatted incorrectly
+    res.status(401).json({ message: "Auth header is not formatted correctly!" });
+    return;
+  }
+  if (!headerComponents[0].startsWith("Bearer")) {
+    // wrong header prefix
+    res.status(401).json({ message: "Incorrect auth header prefix!" });
+    return;
+  }
+  const token = authHeader[1];
   console.log("Token:", token);
-  // TODO verify token
-  next();
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.error(err);
+      res.status(403).json({ message: "Token is invalid or expired!" });
+      return;
+    }
+    console.log("Decoded:", decoded);
+    req.user = decoded;
+    req.is_auth = true;
+    next();
+  });
 };
